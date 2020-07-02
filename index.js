384679d8283e03db131602314c59d447a398884b
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-07-01 22:15:10 -0500

    created db files

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ const massive = require('massive');
 
 const app = express();
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 app.use(express.json());
 
 app.listen( SERVER_PORT, () => {
     console.log(`Server listening on port ${SERVER_PORT}`)
 });
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }).then((db) => {
+      app.set("db", db);
+      console.log("db connected");
+  })
+  .catch(err => console.log(err));
